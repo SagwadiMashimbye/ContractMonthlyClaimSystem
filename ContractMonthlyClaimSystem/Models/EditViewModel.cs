ae4c966ec35d5a3b548928077d5ef443f910c3ec
Sagwadi Mashimbye <ST10168528@rcconnect.edu.za>
2024-11-21 16:11:31 +0000

    Added a new EditViewModel

---
+++ ContractMonthlyClaimSystem/Models/EditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class EditViewModel
+    {
+        public Lecturer? Lecturer { get; set; }
+        public ProgrammeCoordinator? ProgrammeCoordinator { get; set; }
+        public AcademicManager? AcademicManager { get; set; }
+        [Required]
+        public string Role { get; set; } // Add this property
+
+        public List<Lecturer> Lecturers { get; set; } = new List<Lecturer>();
+        public List<ProgrammeCoordinator> ProgrammeCoordinators { get; set; } = new List<ProgrammeCoordinator>();
+        public List<AcademicManager> AcademicManagers { get; set; } = new List<AcademicManager>();
+    }
+}
