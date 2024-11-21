a20b984f530b7af2faf44d45664d3d61186090ff
Sagwadi Mashimbye <ST10168528@rcconnect.edu.za>
2024-11-21 16:16:32 +0000

    Added a new ManageClaimsViewModel

---
+++ ContractMonthlyClaimSystem/Models/ManageClaimsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ContractMonthlyClaimSystem.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class ManageClaimsViewModel
+    {
+        public List<Claims> ApprovedClaims { get; set; } // Ensure this matches the claims model
+        public List<Lecturer> Lecturers { get; set; }
+        public List<ProgrammeCoordinator> ProgrammeCoordinators { get; set; }
+        public List<AcademicManager> AcademicManagers { get; set; }
+        public List<Claims> Claims { get; set; } = new List<Claims>();
+
+        public ManageClaimsViewModel()
+        {
+            ApprovedClaims = new List<Claims>(); // Updated to match the type
+            Lecturers = new List<Lecturer>();
+            ProgrammeCoordinators = new List<ProgrammeCoordinator>();
+            AcademicManagers = new List<AcademicManager>();
+        }
+    }
+}
